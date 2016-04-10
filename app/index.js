73cd732e9c13459fcdf2e10b6aa417b0203946a9
Yifeng Wang <yeefom@gmail.com>
2016-04-10 15:39:16 -0700

    Add Avatar component

--- app/index.js
+++ app/index.js
@@ -1,16 +1,50 @@
+var userData = {
+  name: 'Yifeng',
+  username: 'yeefom',
+  image: 'https://avatars0.githubusercontent.com/u/13666363?v=3&s=460'
+};
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} />;
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function () {
     return (
-      <div>Hello World</div>
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function () {
+    return <div>{this.props.name}</div>;
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     );
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={userData} />,
   document.getElementById('app')
 );
 
