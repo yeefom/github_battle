49d6ceb76cd22e135ac9830af1fea7717ed207ae
Yifeng Wang <yeefom@gmail.com>
2016-04-10 17:34:58 -0700

    Add routes and components

--- app/index.js
+++ app/index.js
@@ -1,50 +1,9 @@
-var userData = {
-  name: 'Yifeng',
-  username: 'yeefom',
-  image: 'https://avatars0.githubusercontent.com/u/13666363?v=3&s=460'
-};
-
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfilePic = React.createClass({
-  render: function () {
-    return <img src={this.props.imageUrl} />;
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <a href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </a>
-      </div>
-    );
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function () {
-    return <div>{this.props.name}</div>;
-  }
-});
-
-var Avatar = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    );
-  }
-});
+var routes = require('./config/routes');
 
 ReactDOM.render(
-  <Avatar user={userData} />,
+  routes,
   document.getElementById('app')
 );
 
