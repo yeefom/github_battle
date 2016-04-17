0addde0dc22f533ff95d8b2c5e4d04fc7bba9e78
Yifeng Wang <yeefom@gmail.com>
2016-04-16 19:04:28 -0700

    Fix a small bug

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -26,11 +26,11 @@ var PromptContainer = React.createClass({
         pathname: '/battle',
         query: {
           playerOne: this.props.routeParams.playerOne,
-          playerTwo: this.state.username
+          playerTwo: username
         }
       });
     } else {
-      this.context.router.push('playerTwo/' + this.state.username);
+      this.context.router.push('playerTwo/' + username);
     }
   },
   render: function() {
