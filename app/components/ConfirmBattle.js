fed19368dd9c251a5c1155ade87a50310f6df5bf
Yifeng Wang <yeefom@gmail.com>
2016-04-17 11:50:46 -0700

    Show raw players info

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,8 +1,14 @@
 var React = require('react');
 
+function puke(obj) {
+  return <pre>{JSON.stringify(obj, null, ' ')}</pre>;
+}
+
 function ConfirmBattle(props) {
   return (
-    props.isLoading === true ? <p> Loading! </p> : <p> Battle! </p>
+    props.isLoading === true
+      ? <p> Loading! </p>
+      : <p> Battle! {puke(props)} </p>
   );
 }
 
