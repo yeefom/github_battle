bd190409f9ca9620996bba58b50127ce3870016e
Yifeng Wang <yeefom@gmail.com>
2016-05-24 21:36:28 -0700

    Add results page

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,51 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
+var Link = require('react-router').Link;
+var MainContainer = require('./MainContainer');
 
-function puke(obj) {
-  return <pre>{JSON.stringify(obj, 2, ' ')}</pre>;
+function StartOver() {
+  return (
+    <Link to='/playerOne'>
+      <button type='button'>Start Over</button>
+    </Link>
+  );
 }
 
 function Results(props) {
+  if (props.isLoading === true) {
+    return (
+      <p>LOADING</p>
+    );
+  }
+
+  if (props.scores[0] === props.scores[1]) {
+    return (
+      <MainContainer>
+        <h1>It's a tie!</h1>
+        <StartOver/>
+      </MainContainer>
+    );
+  }
+
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 :1;
+  var losingIndex = winningIndex === 0 ? 1 : 0;
   return (
-    <div> Results: {puke(props)} </div>
+    <MainContainer>
+      <h1>Results</h1>
+      <div>
+        <UserDetailsWrapper header='Winner'>
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header='Loser'>
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
+        </UserDetailsWrapper>
+      </div>
+      <div>
+        <StartOver/>
+      </div>
+    </MainContainer>
   );
 }
 
